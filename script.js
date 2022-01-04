990df61d39340071f831527b8b67b8225853a68e
Awaiz <owaisshaik99@gmail.com>
2022-01-04 17:46:08 +0530

    added authorization

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ async function callingAPis(reqType, url, data) {
       method: `${reqType}`,
       headers: {
         "Content-Type": "application/json",
+        // "Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImViMWEyZmJlLWI3MjQtNDVjNy05MjE5LTBjZTg5MzY2Yjc3YyIsImlhdCI6MTY0MTI5ODA2MDA2NH0.f0FmYqUzwhqMKIPM3PWvFujETrfglCTi4WNsEsPv6uM',
       },
       body: JSON.stringify(data),
     });
@@ -12,10 +13,24 @@ async function callingAPis(reqType, url, data) {
   }
   
   setInterval(() => {
-    callingAPis("get","https://").then((data) => {   //for post req enter payload ex:{id:1,seller:""}
+    
+    callingAPis("post","http://localhost:7068/api/user/registerUser",
+    {  //body
+      "email": "abc@abc.com",
+      "password": "abc",
+      "firstName": "abc",
+      "lastName": "def",
+      "phone": 12345678990,
+      "registeredWith": "google",
+      "refreshToken": "<some random token from fcm>",
+      "devicePlatform": "android",
+      "deviceId": "SM596",
+      "appleUID": "appleid",
+      "CityId":"00a94649-a85f-43ea-90ff-fad95afe1f1e",
+      "StoreId":"ded80130-5e52-11ec-91cf-3576ffb67b66"
+  }).then((data) => {   //for post req enter payload ex:{id:1,seller:""}
         console.log(data); 
       });
-      
   }, 2000);
 
 
